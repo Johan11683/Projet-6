393fef513cabbe092e0674b180cf41308bf5a9d6
Johan MONARD <monard.johan@yahoo.fr>
2025-04-24 12:08:28 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+
+const app = express();
+
+// Connexion MongoDB
+mongoose.connect('mongodb+srv://monardjohan:13092024Anna!@projet6oc.ctslzqn.mongodb.net/',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((error) => console.error('Connexion à MongoDB échouée !', error));
+
+// CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+// JSON
+app.use(express.json());
+
+
+module.exports = app;
